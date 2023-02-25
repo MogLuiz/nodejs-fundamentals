eabd1d5dbf9bfae32ff114a15db3f8ccfa4c35b6
Luiz Henrique <luizhjramos@outlook.com>
2023-02-25 20:44:30 -0300

    feat: writable stream

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from "node:stream";
+import { Readable, Writable } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -6,13 +6,23 @@ class OneToHundredStream extends Readable {
   _read() {
     const i = this.index++;
 
-    if (i > 100) {
-      this.push(null);
-    } else {
-      const buf = Buffer.from(String(i));
-      this.push(buf);
-    }
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
+        this.push(buf);
+      }
+    }, 1000);
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+// new OneToHundredStream().pipe(process.stdout);
+new OneToHundredStream().pipe(new MultiplyByTenStream());
