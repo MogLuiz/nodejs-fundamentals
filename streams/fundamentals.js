41e2271031eac2545dbf53348698d6ba46807f27
Luiz Henrique <luizhjramos@outlook.com>
2023-02-25 20:59:19 -0300

    feat: transform stream

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable, Writable } from "node:stream";
+import { Readable, Writable, Transform } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -17,6 +17,14 @@ class OneToHundredStream extends Readable {
   }
 }
 
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
 class MultiplyByTenStream extends Writable {
   _write(chunk, encoding, callback) {
     console.log(Number(chunk.toString()) * 10);
@@ -25,4 +33,7 @@ class MultiplyByTenStream extends Writable {
 }
 
 // new OneToHundredStream().pipe(process.stdout);
-new OneToHundredStream().pipe(new MultiplyByTenStream());
+// new OneToHundredStream().pipe(new MultiplyByTenStream());
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
