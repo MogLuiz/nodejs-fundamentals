1f9699af9c97c032e08fb96d73baef0c670694c4
Luiz Henrique <luizhjramos@outlook.com>
2023-02-25 18:15:25 -0300

    feat: initial count nodeJS stream

---
+++ streams/fundamentals.js
@@ -0,0 +1,18 @@
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    if (i > 100) {
+      this.push(null);
+    } else {
+      const buf = Buffer.from(String(i));
+      this.push(buf);
+    }
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
