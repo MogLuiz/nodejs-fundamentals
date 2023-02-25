44ed8b0de0d21d9ccd2dba27380c31ff90b47390
Luiz Henrique <luizhjramos@outlook.com>
2023-02-25 13:54:46 -0300

    feat: logic to create and return users array

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 import http from "http";
 
+const users = [];
+
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users") {
+    return response.end(users)
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: "1",
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+
+    return response.end('Usuário criado com sucesso!')
+  }
+
   return response.end("Hello World");
 });
 
-
-server.listen(3333)
+server.listen(3333);
