46dc6d280d9d0a7dbcb426155300e672f50edd62
Luiz Henrique <luizhjramos@outlook.com>
2023-02-26 10:42:59 -0300

    feat: dynamic user create route with nodeJS stream based in request body

--- src/server.js
+++ src/server.js
@@ -2,8 +2,20 @@ import http from "http";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
+
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    request.body = null;
+  }
 
   if (method === "GET" && url === "/users") {
     return response
@@ -12,10 +24,12 @@ const server = http.createServer((request, response) => {
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = request.body;
+
     users.push({
       id: "1",
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name,
+      email,
     });
 
     return response.writeHead(201).end("Usuário criado com sucesso!");
